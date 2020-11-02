302a7c260a901ea93e95151622d00b8eb9d53637
Ratul Sharker <Sharker.ratul.08@gmail.com>
2020-11-03 00:26:45 +0600

    Empty spring boot project created using `web`, `data-jpa` and `spring boot devtools` dependency.

---
+++ src/main/java/com/foodbuzz/FoodBuzzApplication.java
@@ -0,0 +1,13 @@
+package com.foodbuzz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodBuzzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodBuzzApplication.class, args);
+	}
+
+}
